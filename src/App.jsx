cf5db7cdcd8eaa40cacc9fdb4fe8ec5c11bc57e3
Shahzaib Subhani <shahzaibsubhani28@gmail.com>
2025-08-27 16:50:02 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import EvaluatorRegister from "./pages/EvaluatorRegister";
-import EvaluatorSignIn from "./pages/EvaluatorSignIn";
+
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
