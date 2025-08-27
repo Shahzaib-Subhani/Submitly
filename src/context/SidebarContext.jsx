6d7eeebcee58ac7f72d9c466d78d42f75cef9275
Shahzaib Subhani <shahzaibsubhani28@gmail.com>
2025-08-27 16:49:47 +0400

    Update SidebarContext.jsx

--- src/context/SidebarContext.jsx
+++ src/context/SidebarContext.jsx
@@ -1,10 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useIsMobile } from "../hooks/use-mobile";
 
 export const SidebarContext = createContext();
 
 
 export const SidebarProvider = ({ children }) => {
     const [expanded, setExpanded] = useState(true);
+    const isMobile = useIsMobile();
+
+    useEffect(() => {
+        if (isMobile) setExpanded(false);
+        else setExpanded(true);
+    }, [isMobile]);
 
     return (
         <SidebarContext.Provider value={{ expanded, setExpanded }}>
