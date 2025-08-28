2e36b91fb6cb0e6ffc3e48812bdf8f9a184ad708
Shahzaib Subhani <shahzaibsubhani28@gmail.com>
2025-08-28 16:03:43 +0400

    not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '/404.svg';
 
 const NotFound = () => {
     return (
         <>
-            <>
-                <div className="text-center w-full h-full bg-neutral-300">
-                    <h1 className="text-2xl font-bold">404 Not Found</h1>
+            <div className="relative flex flex-col items-center justify-center min-h-screen p-6 overflow-hidden z-1">
+                <div className="mx-auto w-full max-w-[242px] text-center sm:max-w-[472px]">
+                    <h1 className="mb-8 font-bold text-gray-800 text-title-md dark:text-white/90 xl:text-title-2xl">
+                        ERROR
+                    </h1>
+
+                    <img src={logo} alt="404" />
+
+
+                    <p className="mt-10 mb-6 text-base text-gray-700 dark:text-gray-400 sm:text-lg">
+                        We can’t seem to find the page you are looking for!
+                    </p>
+
+                    <Link
+                        to="/"
+                        className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-5 py-3.5 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 "
+                    >
+                        Back to Home Page
+                    </Link>
                 </div>
-            </>
+                {/* <!-- Footer --> */}
+                <p className="absolute text-sm text-center text-gray-500 -translate-x-1/2 bottom-6 left-1/2 dark:text-gray-400">
+                    &copy; {new Date().getFullYear()} - Submittly
+                </p>
+            </div>
         </>
     );
 }
