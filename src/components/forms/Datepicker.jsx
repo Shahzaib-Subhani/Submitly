d6ad939e7477961384042d9ff08d872693c561a8
Shahzaib Subhani <shahzaibsubhani28@gmail.com>
2025-09-01 16:52:53 +0400

    Datepicker Component

---
+++ src/components/forms/Datepicker.jsx
@@ -0,0 +1,47 @@
+import flatpickr from 'flatpickr';
+import "flatpickr/dist/flatpickr.min.css";
+import { Calendar } from 'lucide-react';
+import React, { useEffect } from 'react';
+
+const Datepicker = ({ id,
+    mode,
+    name,
+    onChange,
+    label,
+    defaultDate,
+    placeholder, }, props) => {
+    useEffect(() => {
+        const flatPickr = flatpickr(`#${id}`, {
+            mode: mode || "single",
+            static: true,
+            monthSelectorType: "static",
+            dateFormat: "d-m-Y",
+            defaultDate,
+            minDate: new Date().fp_incr(1),
+            onChange,
+        });
+
+        return () => {
+            if (!Array.isArray(flatPickr)) {
+                flatPickr.destroy();
+            }
+        };
+    }, [mode, onChange, id, defaultDate]);
+    return (
+        <div className='mb-4 relative'>
+            <label className="block text-sm font-medium text-gray-700 mb-1.5">Choose Date</label>
+
+            <div className="relative">
+                <input
+                    id={id}
+                    name={name}
+                    {...props}
+                    className={`h-11 w-75 rounded-lg border appearance-none px-4 py-2.5 text-md shadow-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3 bg-transparent text-gray-800 border-gray-300 focus:border-indigo-300 focus:ring-indigo-500/20 `}
+                />
+
+            </div>
+        </div>
+    );
+}
+
+export default Datepicker;
