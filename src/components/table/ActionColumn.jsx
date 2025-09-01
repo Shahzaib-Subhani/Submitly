f1f57f22e6d47c12bf2409c68b531ff66da2f96f
Shahzaib Subhani <shahzaibsubhani28@gmail.com>
2025-09-01 12:59:33 +0400

    Flexible Action Button  Component

--- src/components/table/ActionColumn.jsx
+++ src/components/table/ActionColumn.jsx
@@ -1,18 +1,44 @@
-import { Pencil, Trash2 } from 'lucide-react';
+import { Eye, Pencil, Trash2 } from 'lucide-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ActionColumn = () => {
+const ActionColumn = ({
+    isView = false,
+    isEdit = false,
+    isDelete = false,
+    isTextBtn = false,
+    viewPath = "",
+    editPath = "",
+    deletePath = "",
+    textBtnPath = "",
+    textBtnLabel = ""
+}) => {
+
+    const actions = [
+        { enabled: isTextBtn, label: textBtnLabel, path: textBtnPath, hover: "hover:text-indigo-500  hover:bg-indigo-50", path: null },
+        { enabled: isView, label: "View", path: viewPath, icon: <Eye size={20} />, hover: "hover:text-emerald-500  hover:bg-emerald-50" },
+        { enabled: isEdit, label: "Edit", path: editPath, icon: <Pencil size={20} />, hover: "hover:text-blue-500  hover:bg-blue-50" },
+        { enabled: isDelete, label: "Delete", path: deletePath, icon: <Trash2 size={20} />, hover: "hover:text-red-500  hover:bg-red-50" },
+
+    ];
     return (
-        <>
-            <div className="flex items-center w-full gap-2">
-                <button className="text-gray-500 hover:text-red-500  ">
-                    <Trash2 size={20} />
-                </button>
-                <button className="text-gray-500 hover:text-blue-500  ">
-                    <Pencil size={20} />
-                </button>
-            </div>
-        </>
+
+        <div className="flex items-center w-full gap-2">
+            {actions.map(
+                (action, idx) =>
+                    action.enabled && (
+                        <Link
+                            key={idx}
+                            to={action.path}
+                            title={action.label}
+                            className={`text-gray-500 ${action.hover} inline-flex items-center border-1 border-gray-200 justify-center p-1 rounded`}
+                        >
+                            {action.icon ?? action.label}
+                        </Link>
+                    )
+            )}
+        </div>
+
     );
 }
 
