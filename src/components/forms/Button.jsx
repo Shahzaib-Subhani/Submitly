9aaf9b78cb10333780a2e5a99d2204268bb9b831
Shahzaib Subhani <shahzaibsubhani28@gmail.com>
2025-09-03 12:27:09 +0400

    Update Button.jsx

--- src/components/forms/Button.jsx
+++ src/components/forms/Button.jsx
@@ -1,25 +1,25 @@
 import { Loader, LoaderCircle } from "lucide-react";
 import { useState } from "react";
 
-export default function Button({ children, type, className, label, isLoading, ...props }) {
+export default function Button({ children, type, className, label, isLoading, loadingMessage = "Submitting", ...props }) {
   return (
     <button
       type={type}
       className={`py-3 px-4  bg-indigo-500 text-white font-semibold rounded-lg
         shadow-sm hover:bg-indigo-600 transition-colors duration-200
         focus:outline-none focus:ring-1 focus:ring-indigo-100 focus:ring-opacity-50 cursor-pointer
-        disabled:bg-indigo-400
+        disabled:bg-indigo-400 text-center
         ${className}
       `}
       disabled={isLoading}
       {...props}
     >
 
-      {isLoading ? (
+      { isLoading ? (
         <>
-          <span className="ms-2 flex items-center gap-3">
+          <span className="ms-2 flex items-center gap-3 justify-center">
             <Loader size={25} strokeWidth={2} className="animate-spin" />
-            Submitting ...
+            {loadingMessage} ...
           </span>
         </>
       ) : (
