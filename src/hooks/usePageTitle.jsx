a5d9db37e9d7697c8767c30947af2ffad4219710
Shahzaib Subhani <shahzaibsubhani28@gmail.com>
2025-09-03 12:27:04 +0400

    Update usePageTitle.jsx

--- src/hooks/usePageTitle.jsx
+++ src/hooks/usePageTitle.jsx
@@ -6,22 +6,25 @@ const extraMenu = {
     "edit-user": "Edit User",
     "edit-members": "Edit Team Members",
     "edit-evaluator": "Edit Evaluator",
+    "evaluator-signin": "Evaluator Sign In",
+    "evaluator-register": "Evaluator Registration",
+    "team-signin": "Team Sign In",
+    "team-register": "Team Registration",
 }
 
 const usePageTitle = () => {
     const { pathname } = useLocation();
 
     const segments = pathname.split("/").filter(Boolean);
-        
+    
     const roleSegment = segments[0];
     const lastSegment = segments[segments.length - 1];
 
     const title = useMemo(() => {
         const menuItems = sidebarMenu[roleSegment];
-        if (!menuItems) return "Submittly";
+        if (!menuItems) return extraMenu[lastSegment] || "Submittly";
 
         const match = menuItems.find((item) => item.path === lastSegment);
-
         return match?.text || extraMenu[lastSegment] || "Submittly";
     }, [pathname, roleSegment]);
 
