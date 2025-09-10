d70265126919568f62e001d703701ab782d0414b
Shahzaib Subhani <shahzaibsubhani28@gmail.com>
2025-09-10 11:05:52 +0400

    Update ComponentCard.jsx

--- src/components/layout/ComponentCard.jsx
+++ src/components/layout/ComponentCard.jsx
@@ -1,14 +1,20 @@
+import { ArrowLeft, ChevronLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const ComponentCard = ({ title, children, className = "" }) => {
+    const navigate = useNavigate();
     return (
         <>
-            <div className={`rounded-2xl border border-gray-200 bg-white  ${className}`}>
-                <div className="px-6 py-5">
-                    <h3 className="text-lg font-medium text-gray-800">
-                        {title}
-                    </h3>
+            <div className={`rounded-2xl border border-gray-200 bg-white ${className}`}>
+                <div className="flex items-center gap-2 px-6 py-5">
+                    <button
+                        onClick={() => (window.history.length > 1 ? navigate(-1) : navigate("/"))}
+                        className="p-2 rounded-full hover:bg-gray-100"
+                    >
+                        <ChevronLeft size={20} className="w-5 h-5 text-gray-700" />
+                    </button>
+                    <h3 className="text-lg font-medium text-gray-800">{title}</h3>
                 </div>
-
 
                 <div className="p-4 border-t border-gray-100 sm:p-6">
                     <div className="space-y-6">{children}</div>
